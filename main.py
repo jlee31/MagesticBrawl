eaa64b1fea1247bad04da34e5f9334c194a9041c
Joseph Lee <hijosephl31@gmail.com>
2025-07-19 16:10:40 -0400

    basic setup

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    # poll for events  
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill("purple")
+
+    # RENDER YOUR GAME HERE
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()
