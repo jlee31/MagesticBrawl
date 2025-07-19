e0887c61b4f0fa96a5a64a41916ac6820acdd255
Joseph Lee <hijosephl31@gmail.com>
2025-07-19 16:05:47 -0400

    removed files

--- Magestic Brawl/assets/data/tilesets/tileset2.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="tileset2" tilewidth="32" tileheight="32" tilecount="81" columns="9">
- <image source="../graphics/tilemap.png" width="288" height="288"/>
-</tileset>
