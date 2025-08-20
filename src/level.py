954862f7d86e3f9682d0fff51addd1d64f8dbb9d
Lee <Joseph.Lee@ec.gc.ca>
2025-08-20 11:06:16 -0400

    working game

--- src/level.py
+++ src/level.py
@@ -2,6 +2,10 @@ import pygame
 from src.settings import *
 from src.player import *
 
+YELLOW = (255,255,0)
+RED = (255,0,0)
+WHITE = (255,255,255)
+
 class Level:
     def __init__(self):
         # get the display surface
@@ -20,19 +24,30 @@ class Level:
         self.ground_width = self.ground_image.get_width()
         self.ground_height = self.ground_image.get_height()
 
+        # Sprite Sheets
+        self.warrior_sheet = pygame.image.load("assets/images/warrior/warrior.png").convert_alpha()
+        self.warrior_animation_steps = [10,8,1,7,7,3,7]
+        self.sorcerer_sheet = pygame.image.load("assets/images/sorcerer/Sprites/wizard.png").convert_alpha()
+        self.sorcerer_animation_steps = [8,8,1,8,8,3,7]
+
         # Players
-        self.fighter_1 = Fighter2(200,280)
-        self.fighter_2 = Fighter2(400,280)
+        self.fighter_1 = Fighter2(1, 200,280, False, WARRIOR_DATA, self.warrior_sheet, self.warrior_animation_steps)
+        self.fighter_2 = Fighter2(2, 400,280, True, SORCERER_DATA, self.sorcerer_sheet, self.sorcerer_animation_steps)
 
     
     def run(self, dt):
-        self.fighter_1.move()
-        self.fighter_2.move()
+        self.fighter_1.move(target=self.fighter_2)
+        self.fighter_2.move(target=self.fighter_1)
+        self.fighter_1.animation_update()
+        self.fighter_2.animation_update()
         self.fighter_1.draw(self.display_surface)
         self.fighter_2.draw(self.display_surface)
 
+        # Player Stats
+        self.drawHealthBar(self.fighter_1.health, 20, 20)
+        self.drawHealthBar(self.fighter_2.health, 410,20)
+
     def drawBg(self):
-        
         for x in range(3):
             speed = 1
             for i in self.bgImages:
@@ -56,8 +71,11 @@ class Level:
         if self.scroll > 1000:
             self.scroll = 0
                
-    def drawHealthBar(self):
-        pass
+    def drawHealthBar(self, health, x, y):
+        ratio = health / 100
+        pygame.draw.rect(self.display_surface, WHITE, (x - 2,y - 2, 304, 34))
+        pygame.draw.rect(self.display_surface, RED, (x,y, 300, 30) )
+        pygame.draw.rect(self.display_surface, YELLOW, (x,y, 300 * ratio, 30))
     
     def drawText(self):
         pass
